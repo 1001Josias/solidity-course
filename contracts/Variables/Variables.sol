fcadc8d5f86a4f9b763f7c568335c176bae824cc
Josias Junior <1001josias@gmail.com>
2023-02-20 16:51:05 -0300

    Understanding variables in solidity

---
+++ contracts/Variables/Variables.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+contract Variables {
+    string public publicText = "This text is visible";
+    string internal internalText = "This text not is visible";
+}
