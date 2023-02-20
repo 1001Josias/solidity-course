417e47d356a95a6d7c31138f98612f9cac2dc4f8
Josias Junior <1001josias@gmail.com>
2023-02-20 17:28:58 -0300

    Function with public visibility

---
+++ contracts/Function/Visibility.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+contract Visibility {
+    int score = 0;
+
+    function increment() public {
+        score = score + 1;
+    }
+}
